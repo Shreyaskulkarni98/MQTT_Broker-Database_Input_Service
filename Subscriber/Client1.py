a040bbfa6f596aaaad3a962a12fbdf42f9234229
Shreyas Kulkarni <Shreyasrameshkulkarni@gmail.com>
2018-12-10 18:04:11 +0530

    Added Exception handelling to database connections

--- Subscriber/Client1.py
+++ Subscriber/Client1.py
@@ -3,40 +3,49 @@ import logging
 import datetime
 import MySQLdb
 
-
 logging.basicConfig(filename='example.log', filemode='w', level=logging.INFO)
-conn = MySQLdb.connect(host='localhost', user='root', passwd='Kulkarni10')
-curs = conn.cursor()
 
-def on_subscribe(client, userdata, mid, granted_qos):
-#    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-    logging.info(str(datetime.datetime.now().strftime("%c")) + ' Client is now subscribed to broker')
-    curs.execute('use test_database')
+try:
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='Kulkarni10')
+    curs = conn.cursor()
 
-
-def on_message(client, userdata, msg):
-    print "Message recieved"
-#    print(msg.topic + " QoS Of Message=" + str(msg.qos) + " \nMessage: " + str(msg.payload))
-    logging.info(str(datetime.datetime.now().strftime("%c")) + " Client has recieved a message")
-    curs.execute("insert into mqttbroker values(now(),%s,%s,%s)", [str(msg.topic), str(msg.qos), str(msg.payload)])
-    conn.commit()
-    logging.info(str(datetime.datetime.now().strftime("%c")) + " Message has been committed to database")
+    def on_subscribe(client, userdata, mid, granted_qos):
+    #    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+        logging.info(str(datetime.datetime.now().strftime("%c")) + ' Client is now subscribed to broker')
+        curs.execute('use test_database')
 
 
-if __name__ == '__main__':
-    client = paho.Client()
-    #Subscribing to a broker
-    client.on_subscribe = on_subscribe
-    logging.info("Before on message()")
-    client.on_message = on_message
+    def on_message(client, userdata, msg):
+        print "Message recieved"
+    #    print(msg.topic + " QoS Of Message=" + str(msg.qos) + " \nMessage: " + str(msg.payload))
+        logging.info(str(datetime.datetime.now().strftime("%c")) + " Client has recieved a message")
 
-    #Connecting to broker
-    client.connect("test.mosquitto.org", 1883)
-    client.subscribe("topic1/subtopic/sub2topic/sub3topic", qos=1)
+        try:
+            curs.execute("insert into mqttbroker values(now(),%s,%s,%s)", [str(msg.topic), str(msg.qos), str(msg.payload)])
+            conn.commit()
+        except(MySQLdb.Error, MySQLdb.Warning) as e:
+            print(e)
 
-    while not raw_input()=='exit':
-        client.loop()
+        logging.info(str(datetime.datetime.now().strftime("%c")) + " Message has been committed to database")
+
+
+    if __name__ == '__main__':
+        client = paho.Client()
+        #Subscribing to a broker
+        client.on_subscribe = on_subscribe
+        logging.info("Before on message()")
+        client.on_message = on_message
+
+        #Connecting to broker
+        client.connect("test.mosquitto.org", 1883)
+        client.subscribe("topic1/subtopic/sub2topic/sub3topic", qos=1)
+
+        while not raw_input()=='exit':
+            client.loop()
 
     client.loop_stop()
     client.disconnect()
+
+finally:
     conn.close()
+

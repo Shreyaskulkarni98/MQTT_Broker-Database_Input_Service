e5514fe068d96c965d49f898693c4a64a741bba7
Shreyas Kulkarni <Shreyasrameshkulkarni@gmail.com>
2018-12-12 10:26:46 +0530

    Removed weak warnings for publisher1

--- Publisher/Publisher1.py
+++ Publisher/Publisher1.py
@@ -4,15 +4,17 @@ import datetime
 import random
 import time
 
-logging.basicConfig(filename='example.log',filemode='w', level=logging.INFO)
+logging.basicConfig(filename='example.log', filemode='w', level=logging.INFO)
 
 def on_connect(client, userdata, flags, rc):
-#    print "CONNACK received with code % d." % (rc)
+    # print "CONNACK received with code % d." % (rc)
     logging.info(str(datetime.datetime.now().strftime("%c")) + ' Broker is now connected')
+
 
 def on_publish(client, userdata, mid):
     print("Message "+str(mid)+" sent")
     logging.info(str(datetime.datetime.now().strftime("%c")) + ' Broker has now published')
+
 
 if __name__ == '__main__':
     client = paho.Client()
@@ -21,7 +23,7 @@ if __name__ == '__main__':
     client.connect("test.mosquitto.org", 1883, keepalive=60)
     WORDS = ("Apples", "Bananas", "Carrots", "Oranges", "Mangos", "Cranberry")
     client.loop_start()
-    for x in range(0,5):
+    for x in range(0, 5):
         word = random.choice(WORDS)
         client.publish("topic1/subtopic/sub2topic/sub3topic", word, qos=1, retain=False)
         time.sleep(3)

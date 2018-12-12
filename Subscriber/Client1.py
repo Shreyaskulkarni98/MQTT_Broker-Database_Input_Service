17d7cf9ea67ff135eeeedef9245dc37c314b77ec
Shreyas Kulkarni <shreyasrameshkulkarni@gmail.com>
2018-12-12 10:21:45 +0530

    Removed weak warnings for client1

--- Subscriber/Client1.py
+++ Subscriber/Client1.py
@@ -10,14 +10,14 @@ try:
     curs = conn.cursor()
 
     def on_subscribe(client, userdata, mid, granted_qos):
-    #    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+        # print("Subscribed: " + str(mid) + " " + str(granted_qos))
         logging.info(str(datetime.datetime.now().strftime("%c")) + ' Client is now subscribed to broker')
         curs.execute('use test_database')
 
 
     def on_message(client, userdata, msg):
         print "Message recieved"
-    #    print(msg.topic + " QoS Of Message=" + str(msg.qos) + " \nMessage: " + str(msg.payload))
+        # print(msg.topic + " QoS Of Message=" + str(msg.qos) + " \nMessage: " + str(msg.payload))
         logging.info(str(datetime.datetime.now().strftime("%c")) + " Client has recieved a message")
 
         try:
@@ -31,20 +31,20 @@ try:
 
     if __name__ == '__main__':
         client = paho.Client()
-        #Subscribing to a broker
+        # Subscribing to a broker
         client.on_subscribe = on_subscribe
         logging.info("Before on message()")
         client.on_message = on_message
 
-        #Connecting to broker
+        # Connecting to broker
         client.connect("test.mosquitto.org", 1883)
         client.subscribe("topic1/subtopic/sub2topic/sub3topic", qos=1)
 
-        while not raw_input()=='exit':
+        while not raw_input() == 'exit':
             client.loop()
 
-    client.loop_stop()
-    client.disconnect()
+        client.loop_stop()
+        client.disconnect()
 
 finally:
     conn.close()
